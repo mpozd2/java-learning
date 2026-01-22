03efc093d0c5f101aa4df8cd1d647cd7c823e7b8
M.Pozd <mpozd237@gmail.com>
2026-01-22 15:30:17 +0200

    [feature]/Java_lesson3

---
+++ src/main/java/lv/acodemy/objects/Car.java
@@ -0,0 +1,13 @@
+package lv.acodemy.objects;
+
+public class Car {
+    //Fields
+    private String color;
+    private String model;
+
+    public void start() {
+        System.out.println("Car started");
+
+    }
+
+}
